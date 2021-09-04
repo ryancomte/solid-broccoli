30c3a7d1fafcf9741d5ed63a97f4ce8da654683b
Ryan Comte <ryancomte@gmail.com>
2021-09-04 13:06:16 -0600

    Heroku updates

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -10,6 +10,8 @@ const { authMiddleware } = require('./utils/auth');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+require('dotenv').config();
+
 let server = null;
 
 async function startServer() {
@@ -32,6 +34,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 // app.use(routes);
 
