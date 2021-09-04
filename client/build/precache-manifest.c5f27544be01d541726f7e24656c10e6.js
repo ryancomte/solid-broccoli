efd8bc80e82cada489c94834cca45ff8031005f5
Ryan Comte <ryancomte@gmail.com>
2021-09-04 14:46:55 -0600

    Adding build folder

---
+++ client/build/precache-manifest.c5f27544be01d541726f7e24656c10e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb43e8cbb67a49b2c1e899cec6a51668",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce80b23f5c0023ed5f5",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "1928031732dd8ae31a1f",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "dce80b23f5c0023ed5f5",
+    "url": "/static/js/2.2b1b5c6b.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.2b1b5c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1928031732dd8ae31a1f",
+    "url": "/static/js/main.aef1cc7d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
