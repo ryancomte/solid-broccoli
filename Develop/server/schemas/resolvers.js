0e5bb7fd6729086ecccce19d014a756dff32b66d
Ryan Comte <ryancomte@gmail.com>
2021-09-03 18:42:28 -0600

    Added apollo server, mutations & resolvers

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,78 @@
+const { AuthenticationError } = require('apollo-server-express');
+
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({})
+          .select('-__v -password')
+          .populate('savedBooks');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const book = await Book.create({
+          ...args,
+          username: context.user.username,
+        });
+
+        const updatedUser = await User.findOneAndUpdate(
+          {
+            _id: context.user._id,
+          },
+          { $addToSet: { books: book } },
+          { new: true }
+        ).populate('books');
+
+        return updatedUser;
+      }
+    },
+    removeBook: async (parent, { bookId }) => {
+      if (context.user) {
+        const updateUser = await User.findOneAndUpdate(
+          {
+            _id: context.user._id,
+          },
+          { $pull: { books: { _id: bookId } } },
+          { multi: false }
+        );
+
+        return updateUser;
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
